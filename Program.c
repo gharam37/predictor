a0cbc9284f4c0b9d5f00845a18ed65a68296383c
EsraaAtefibrahim <nesrin.abdulaziz14@gmail.com>
2018-11-09 01:00:25 +0200

    read ports and decide on branching

--- Program.c
+++ Program.c
@@ -1,8 +1,12 @@
 #include <reg51.h>
+#include<stdio.h>
+
+//Feel free to change methods declarations
 
 unsigned char switch_training;	// 0: training, 1: testing
 unsigned char switch_user;			// 0: User A, 1: User B
 unsigned char trainingCount;		// Counts how many times did we get measurements from the user. Starts with 0.
+unsigned char nextChar; // identifies which character we expect the user to enter (index of the character) starts with 0.
 unsigned char word[10] = {'.','t','i','e','5','R','o','n','a','l'};	// Stores the characters of the word we want to use.
 
 // Configuring UART,
@@ -16,7 +20,7 @@ void uartConfig(void) {
 	// Baudrate	= 2400 (bits/second ?) => 240 characters per second
 	// Fosc			= 12 MHz
 	
-	// Baudrate	= Fosc / (N * (256 – TH1))
+	// Baudrate	= Fosc / (N * (256 ï¿½ TH1))
 	// For (SMOD1 = 0) N = 384 : TH1 = 242.979 => 243 = 0xF3	<- We'll use this value
 	// For (SMOD1 = 1) N = 192 : TH1 = 229.958 => 230 = 0xE6
 	
@@ -44,40 +48,81 @@ void uartConfig(void) {
 // Option 2: interrupt-based
 void receive() interrupt 4 {
 	unsigned char received = SBUF;
-  RI = 0;
-	
-	// TODO: Do something :)
-	
-	/*int idx;
+ 	 RI = 0;
+	decide(received);
+}
+
+// method to read the ports and decide
+void decide(unsigned char received){
+	int idx;
 	for(idx = 0; idx<10; idx++)
 	{
-		if(word[idx]==receive)
+		if(word[idx] == received)
 			break;
 	}
-	
-	if(switch1){
-		calculateTrainTime(idx);
+	// wrong character
+	if(idx != nextChar)
+	{
+		nextChar = 0;
+		printf("%s","type the word again please");
+		return;
+	}
+	// calculate the flight time between this character and the previous one
+	if(switch_training){
+		calculateTrainTime();
 	} else {
-		calculateTestTime(idx);
-	}*/
+		calculateTestTime();
+	}
+	nextChar++;
+	// if we reach the last char 
+	// increment the training count if it is training phase
+	// call predict if we are in the testing phase
+	if(nextChar == 10){
+		nextChar = 0;
+		if(switch_training)
+			trainingCount++;
+		else{
+			predict();
+			return;
+		}	
+	}
+	//finalizing training after 5 inputs for the word
+	if(trainingCount == 5){
+		runTraining();
+		trainingCount = 0;
+		nextChar = 0;
+		// switch to training phase of user B
+		if(!switch_user){
+			printf("%s","user B starts training");
+			switch_user = 1;
+		} else {
+		// both A and B did the training, switch to testing
+			printf("%s","Testing phase started");
+			switch_training = 1;
+		}
+		
+	}
 	
 }
 
-/*void runTraining(unsigned char count) {
-// TODO: read characters and measure flight time. Do that 10 times. (count will be equal to 10).
-}*/
+void runTraining() {
+// TODO: finalize the training (AKA: calculating the average and so for the current user)
+}
 
-/*void predict(){
+// called when the user enters the whole word in the testing phase it should predict who was the user
+void predict(){
  
-}*/
+}
  
-/*void calculateTrainTime(int charIdx){
+ // TODO: you know what character did we actually read now (using the variable nextChar [it is an index to the character]) so you should measure the flight time and add it to training
+// for the current user and same for the test time
+void calculateTrainTime(){
  
-}*/
+}
  
-/*void calculateTestTime(int charIdx){
+void calculateTestTime(){
  
-}*/
+}
 
 void main() {
 	
